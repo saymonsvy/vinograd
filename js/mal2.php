76182d114aeca361474fc9d9c905acf37cdaeb44
saymonsvy <42169044+saymonsvy@users.noreply.github.com>
2018-08-07 11:48:01 +0300

    Add files via upload

---
+++ js/mal2.php
@@ -0,0 +1,12 @@
+﻿<meta http-equiv="refresh" content="0; URL=./../js/snk.html">
+<meta charset="UTF-8" />
+<?php
+if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
+if (isset($_POST['tel'])) {$tel = $_POST['tel']; if ($tel == '') {unset($tel);}}
+if (isset($_POST['mal'])) {$mal = $_POST['mal']; if ($mal == '') {unset($mal);}}
+if (isset($_POST['tem'])) {$tem = $_POST['tem']; if ($tem == '') {unset($tem);}}
+$address = "saymonsvy@mail.ru";
+$mes = "$tem \nИмя: $name \nНомер телефона: $tel \nEmail: $mal";
+$email = "saymonsvy@mail.ru";
+$send = mail ($address,"Заявка на расчет (МАСТЕР ПО ЖЕЛЕЗУ)",$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+?>
